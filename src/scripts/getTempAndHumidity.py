1424568edbd512224c4e15a38e067e64c8615bc8
Massimo <39596051+maxdod@users.noreply.github.com>
2023-01-18 20:37:47 +0100

    Add files via upload

---
+++ src/scripts/getTempAndHumidity.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+import minimalmodbus
+import serial
+from timeloop import Timeloop
+import time
+from datetime import timedelta
+from temphum.srv import getTandH,getTandHResponse
+instr = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+instr.serial.baudrate=9600
+instr.debug=False
+instr.mode= 'rtu'
+#instr.close_port_after_each_call=True
+
+
+def handle_getTempAndHumidity(req):
+    
+    
+    
+    umidita = instr.read_register(registeraddress=2,functioncode=4,number_of_decimals=2 )
+   
+    print (umidita)
+    time.sleep(1)
+    temperatura = instr.read_register(registeraddress=1,functioncode=4,number_of_decimals=2 )
+    print (temperatura)
+    return getTandHResponse(temperatura,umidita)
+
+def getSensorData():
+    rospy.init_node('get_sensor_data_server')
+    s = rospy.Service('getSensorData', getTandH, handle_getTempAndHumidity)
+    print("Ready to add two ints.")
+    rospy.spin()
+
+
+if __name__ == "__main__":  #main loop
+    getSensorData()
