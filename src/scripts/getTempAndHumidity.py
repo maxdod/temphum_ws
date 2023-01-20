3c5bad31f07b2e38cbd672499c019a396a00871d
Massimo <39596051+maxdod@users.noreply.github.com>
2023-01-20 18:09:31 +0100

    Add files via upload
    added exception for false behaviors reading from register

--- src/scripts/getTempAndHumidity.py
+++ src/scripts/getTempAndHumidity.py
@@ -2,32 +2,44 @@
 import rospy
 import minimalmodbus
 import serial
+from timeloop import Timeloop
 import time
 from datetime import timedelta
 from temphum.srv import getTandH,getTandHResponse
 instr = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
 instr.serial.baudrate=9600
-instr.debug=False
+instr.debug=True
 instr.mode= 'rtu'
+instr.clear_buffers_before_each_transaction = True
 #instr.close_port_after_each_call=True
 
 
 def handle_getTempAndHumidity(req):
     
     
+    #time.sleep(2) # sleep 1 secondo 
+    try:
+         umidita = instr.read_register(registeraddress=2,functioncode=4,number_of_decimals=2 )
+    except minimalmodbus.NoResponseError:
+         time.sleep(2)
+         umidita = instr.read_register(registeraddress=2,functioncode=4,number_of_decimals=2 )
     
-    umidita = instr.read_register(registeraddress=2,functioncode=4,number_of_decimals=2 )
    
-    print (umidita)
+    #print (umidita)
+    
     time.sleep(1)
-    temperatura = instr.read_register(registeraddress=1,functioncode=4,number_of_decimals=2 )
-    print (temperatura)
+    try:
+        temperatura = instr.read_register(registeraddress=1,functioncode=4,number_of_decimals=2 )
+    except minimalmodbus.NoResponseError: 
+        time.sleep(1)
+        temperatura = instr.read_register(registeraddress=1,functioncode=4,number_of_decimals=2 )   
+        print (temperatura)
     return getTandHResponse(temperatura,umidita)
 
 def getSensorData():
     rospy.init_node('get_sensor_data_server')
     s = rospy.Service('getSensorData', getTandH, handle_getTempAndHumidity)
-    print("Ready to return temperature & humidity")
+    print("Ready to get temperature and humidity")
     rospy.spin()
 
 
